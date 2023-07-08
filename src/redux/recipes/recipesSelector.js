d107cf00ad46c69dd89cbf829218a121873b7ede
Daniil Popov <x6uhrox@gmail.com>
2023-07-08 21:49:27 +0300

    FIX: recipe by recipe, ingredient by ingredient in redux

--- src/redux/recipes/recipesSelector.js
+++ src/redux/recipes/recipesSelector.js
@@ -1,3 +1,2 @@
 export const selectCategoryList = state => state.recipes.categoryList;
-export const selectIngredientsList = state => state.recipes.ingredientsList;
 export const selectMainPageRecipes = state => state.recipes.mainPageRecipes;
