b5d99727ad033e45a755a3b3a58a067ad7d43197
Daniil Popov <x6uhrox@gmail.com>
2023-07-10 21:55:38 +0300

    ADDED: getOwnRecipe and deleteOwnRecipe

--- src/redux/recipes/operations/index.js
+++ src/redux/recipes/operations/index.js
@@ -2,11 +2,15 @@ import getCategoryList from "./getCategoryList";
 import getMainPageRecipes from "./getMainPageRecipes";
 import getRecipesById from "./getRecipesById";
 import getRecipesByTitle from "./getRecipesByTitle";
+import deleteownRecipe from "./deleteOwnRecipe";
+import getOwnRecipe from "./getOwnRecipe";
 
  export const recipeOperations = {
     getCategoryList,
     getMainPageRecipes,
     getRecipesById,
     getRecipesByTitle,
+    deleteownRecipe,
+    getOwnRecipe,
 }
 
