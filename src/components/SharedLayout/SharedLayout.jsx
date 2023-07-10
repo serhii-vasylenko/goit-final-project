dc658f17d11415d5f61d83d241662c9d2a9f3c69
Jerrssey <fankton@gmail.com>
2023-07-10 20:09:30 +0300

    ADDED: layout wrapper

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,17 +2,22 @@ import { Footer } from 'components/Footer/Footer';
 import { Header } from 'components/Header/Header';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
+import { MainContainer } from './SharedLayout.styled';
 
 const SharedLayout = () => {
-  return <>
-      <Header />
-    <main>
-        <Suspense fallback={<div>Loading</div>}>
-          <Outlet />
-        </Suspense>
-      </main>
-      <Footer/>
+  return (
+    <>
+      <MainContainer>
+        <Header />
+        <main>
+          <Suspense fallback={<div>Loading</div>}>
+            <Outlet />
+          </Suspense>
+        </main>
+        <Footer />
+      </MainContainer>
     </>
+  );
 };
 
 export default SharedLayout;
