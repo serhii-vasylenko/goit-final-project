59e65386dd34a13c8a3a755e79145e3714466d3f
Daniil Popov <x6uhrox@gmail.com>
2023-07-10 21:32:27 +0300

    FIX: byTitle func

--- src/redux/recipes/operations/getRecipesByTitle.js
+++ src/redux/recipes/operations/getRecipesByTitle.js
@@ -5,7 +5,7 @@ const getRecipesByTitle = createAsyncThunk(
   'recipes/getRecipesByTitle',
   async (title, thunkAPI) => {
     try {
-      const response = await axios.get(`/search/q=${title}`);
+      const response = await axios.get(`/search?q=${title}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
