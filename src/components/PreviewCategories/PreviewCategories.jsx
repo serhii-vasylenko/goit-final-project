a29805d72d718f3361fc4eda7efbfb9ea271500e
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-07-11 18:03:35 +0300

    UPDATE

--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -1,26 +1,42 @@
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const PreviewCategories = () => (
-  <section>
-        <ul>
-          <li>
-            <h2>Breakfast</h2>
-            <Link>See all</Link>
-          </li>
-          <li>
-            <h2>Miscellaneous</h2>
-            <Link>See all</Link>
-          </li>
-          <li>
-            <h2>Vegan</h2>
-            <Link>See all</Link>
-          </li>
-          <li>
-            <h2>Desserts</h2>
-            <Link>See all</Link>
-          </li>
-        </ul>
-      </section>
-);
+// import { selectMainPageRecipes } from 'redux/recipes/recipesSelector';
+// import getMainPageRecipes from 'redux/recipes/operations/getMainPageRecipes';
+
+const PreviewCategories = () => {
+  // const dispatch = useDispatch();
+  // const categories = useSelector(selectMainPageRecipes);
+
+  // useEffect(() => {
+  //   dispatch(getMainPageRecipes());
+  // }, [dispatch]);
+
+  return (
+    <section>
+      <ul>
+        <li>
+          <h2>Breakfast</h2>
+          <Link>See all</Link>
+        </li>
+        <li>
+          <h2>Miscellaneous</h2>
+          <Link>See all</Link>
+        </li>
+        <li>
+          <h2>Vegan</h2>
+          <Link>See all</Link>
+        </li>
+        <li>
+          <h2>Desserts</h2>
+          <Link>See all</Link>
+        </li>
+      </ul>
+      {/* <button type="button" onClick={()=>{dispatch(getMainPageRecipes())}}>ggggg</button>/ */}
+    </section>
+    
+  );
+};
 
 export default PreviewCategories;
