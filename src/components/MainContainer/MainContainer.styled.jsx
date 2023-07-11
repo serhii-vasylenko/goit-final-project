cf2a649d9f7c0bab6a7e59a931e49be8c591141c
Jerrssey <fankton@gmail.com>
2023-07-11 23:29:41 +0300

    styles

---
+++ src/components/MainContainer/MainContainer.styled.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  min-width: 320px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 16px;
+  padding-left: 16px;
+  @media screen and (min-width: 375px) {
+    width: 375px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 768px;
+    padding-right: 32px;
+    padding-left: 32px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+    padding-right: 20px;
+    padding-left: 20px;
+  }
+`;
--- src/pages/NotFoundPage/NotFound.jsx
+++ src/pages/NotFoundPage/NotFound.jsx
@@ -1,7 +1,41 @@
-const NotFound = () => {
-return <>
-oops
-</>
-}
+import { Container } from 'components/MainContainer/MainContainer.styled';
 
-export default NotFound;
+import notFoundImageMob from 'images/notFoundPage/notFoundImage-mob.png';
+import notFoundImageMob2x from 'images/notFoundPage/notFoundImage-mob@2x.png';
+import notFoundImageTab from 'images/notFoundPage/notFoundImage-tab.png';
+import notFoundImageTab2x from 'images/notFoundPage/notFoundImage-tab@2x.png';
+import notFoundImagePc from 'images/notFoundPage/notFoundImage-pc.png';
+import notFoundImagePc2x from 'images/notFoundPage/notFoundImage-pc@2x.png';
+import { PictureWrapper, Section, Text, TextWrapper, Title } from './NotFound.styled';
+
+const NotFoundPage = () => {
+  return (
+    <Section>
+      <Container>
+        <PictureWrapper>
+          <picture>
+            <source
+              media="(min-width: 1240px)"
+              srcSet={`${notFoundImagePc}, ${notFoundImagePc2x} 2x`}
+            />
+            <source
+              media="(min-width: 768px)"
+              srcSet={`${notFoundImageTab}, ${notFoundImageTab2x} 2x`}
+            />
+            <img
+              src={notFoundImageMob}
+              srcSet={`${notFoundImageMob}, ${notFoundImageMob2x} 2x`}
+              alt="Not Found Page"
+            />
+          </picture>
+        </PictureWrapper>
+        <TextWrapper>
+          <Title>We are sorry,</Title>
+          <Text>but the page you were looking for can’t be found..</Text>
+        </TextWrapper>
+      </Container>
+    </Section>
+  );
+};
+
+export default NotFoundPage;
