31f605ea0fae1ef2545f1e2fbd1deebb56c2f059
serhii-vasylenko <113785494+serhii-vasylenko@users.noreply.github.com>
2023-07-12 14:30:01 +0300

    Update RecipePageHero.jsx

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import { recipeHeroWrapper } from './RecipePageHero.styled'
 
-const RecipePageHero = () => {
+const RecipePageHero = ({children}) => {
   return (
      <>
-        <recipeHeroWrapper/>     
+        <recipeHeroWrapper>{children}<recipeHeroWrapper/>      
     </>
   )
 }
