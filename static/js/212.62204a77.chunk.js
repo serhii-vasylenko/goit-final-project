970046c89288c2705c5105887c75ea6dd87c8ee5
serhii-vasylenko <113785494+serhii-vasylenko@users.noreply.github.com>
2023-07-14 08:47:46 +0000

    Deploying to gh-pages from @ serhii-vasylenko/goit-final-project@7c1cc978a3f3f0e9b92a33f825bcd75f7301cbe2 🚀

--- static/js/212.62204a77.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[212],{9212:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{})}}}]);
-//# sourceMappingURL=212.62204a77.chunk.js.map
