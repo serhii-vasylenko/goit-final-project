c108c746e12c229b580adf7e044326d988669245
Jerrssey <fankton@gmail.com>
2023-07-16 18:00:26 +0300

    ADDED: common styles

--- src/themes/darkTheme.js
+++ src/themes/darkTheme.js
@@ -1,5 +1,19 @@
 export const darkTheme = {
     colors: {
       primary: 'green',
+      accentColor: '#8aa936',
+      whiteColor: '#fafafa',
+      backgroundColor: '#fafafa',
+      primaryTextColor: '#23262a',
+      secondaryTextColor: '#3e4462',
+      pageTitleColor: '#001833',
+      backgroundColorCircle: '#22252a',
+      backgroundAuthFormColor: '#2a2c3',
+      inputBorderColor: '#e0e0e0',
+      errorColor: '#e74a3b',
     },
-  }
+    transforms: {
+      transitionDuration: '250ms',
+      timingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    },
+  };
--- src/themes/lightTheme.js
+++ src/themes/lightTheme.js
@@ -1,5 +1,46 @@
 export const lightTheme = {
-    colors: {
-      primary: 'tomato',
-    },
-  }
+  colors: {
+    primary: 'tomato',
+    accentColor: '#8aa936',
+    whiteColor: '#fafafa',
+    backgroundColor: '#fafafa',
+    primaryTextColor: '#23262a',
+    secondaryTextColor: '#3e4462',
+    pageTitleColor: '#001833',
+    backgroundColorCircle: '#22252a',
+    backgroundAuthFormColor: '#2a2c3',
+    inputBorderColor: '#e0e0e0',
+    errorColor: '#e74a3b',
+  },
+  transforms: {
+    transitionDuration: '250ms',
+    timingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+};
+
+/*
+:root {
+  --accent-color: #8aa936;+
+  --white-color: #fafafa;+
+  --;
+  --;
+  --background-color: #fafafa;+
+  --primary-text-color: #23262a;+
+  --;
+  --;
+  --;
+  --
+  --;
+  --
+  --background-color: #fafafa;
+  --primary-text-color: #23262a;
+  --secondary-text-color: #3e4462;
+}
+:root[data-theme='light'] {
+  --background-color: #fafafa;
+  --primary-text-color: #23262a;
+  --secondary-text-color: #3e4462;
+}
+
+
+  */
