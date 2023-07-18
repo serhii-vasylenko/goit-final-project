312d175fa1311f2c4c2f6ee5e69dfb91319f869a
Yulia Osnova <yulia.osnova@gmail.com>
2023-07-18 19:20:07 +0300

    FIXED: work with shopping-list by Yulia Osnova

--- src/components/RecipeInngredientsList/CustomCheckbox.jsx
+++ src/components/RecipeInngredientsList/CustomCheckbox.jsx
@@ -1,47 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addToShoppingList, removeFromShoppingList } from 'redux/shopping-list/operations';
-import { CheckBox} from './RecipeInngredientsList.styled'
+import {
+  addToShoppingList,
+  removeFromShoppingList,
+} from 'redux/shopping-list/operations';
+import { CheckBox } from './RecipeInngredientsList.styled';
 
 const CustomCheckbox = ({ ingredient, shoppingList, recipeId }) => {
-   // console.log(ingredient);
-   // console.log(recipeId)
-   // console.log(shoppingList);
-   
-   const [checked, setChecked] = useState(false);
-   const dispatch = useDispatch();
-   const { _id, measure } = ingredient;
 
-//    useEffect(() => {
-//     if (shoppingList.length > 0) {
-//       const inShoppingList = shoppingList.filter(
-//         item => item.ingredientId === _id
-//       );
-//        setChecked(inShoppingList ? inShoppingList.id : false);
-       
-//     }
-//   }, [shoppingList, dispatch, _id]);
+  const [checked, setChecked] = useState(false);
+  const dispatch = useDispatch();
+  const { _id: ingredientId, measure} = ingredient;
 
+  //    useEffect(() => {
+  //     if (shoppingList.length > 0) {
+  //       const inShoppingList = shoppingList.filter(
+  //         item => item.ingredientId === _id
+  //       );
+  //        setChecked(inShoppingList ? inShoppingList.id : false);
 
-  const toggleCheckBox = ( product) => {
-    if (checked) {
-       dispatch(removeFromShoppingList(checked))
-       setChecked(false);
+  //     }
+  //   }, [shoppingList, dispatch, _id]);
+
+  //   const toggleCheckBox = ( product) => {
+  //     if (checked) {
+  //        dispatch(removeFromShoppingList(checked))
+  //        setChecked(false);
+  //     } else {
+  //        dispatch(addToShoppingList(product))
+  //        setChecked(true);
+  //     }
+  //   };
+
+  const toggleCheckBox = ({ measure, ingredientId, recipeId }) => {
+    if (shoppingList.length === 0) return;
+
+    const alreadyInSL = shoppingList.filter(item => item._id === ingredientId);
+    //   (item._id === ingredientId && item.recipeId === recipeId)
+
+    if (alreadyInSL.length > 0) {
+      
+      dispatch(
+        removeFromShoppingList({
+          ingredientId: ingredientId,
+          recipeId: recipeId,
+          measure: measure,
+        })
+      );
+      setChecked(false);
     } else {
-       dispatch(addToShoppingList(product))
-       setChecked(true);
+   
+      dispatch(
+        addToShoppingList({
+          ingredientId: ingredientId,
+          recipeId: recipeId,
+          measure: measure,
+        })
+      );
+      setChecked(true);
     }
   };
-   
-  return (
-     <CheckBox
-        type="checkbox"
-        checked={checked}
-      onChange={() => toggleCheckBox({ measure, ingredientId:_id, recipeId})}
-    >
-     
-    </CheckBox>
-  );
-}
 
-export default CustomCheckbox
+  return (
+    <CheckBox
+      type="checkbox"
+      checked={checked}
+      onChange={() => toggleCheckBox({ measure, ingredientId, recipeId })}
+    ></CheckBox>
+  );
+};
+
+export default CustomCheckbox;
--- src/redux/shopping-list/shoppingListSlice.js
+++ src/redux/shopping-list/shoppingListSlice.js
@@ -38,10 +38,8 @@ const shoppingListSlice = createSlice({
       .addCase(addToShoppingList.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        state.products.shoppingList = [
-          ...state.shoppingList,
-          action.payload.data.shoppingList,
-        ];
+		//   console.log("PAYLOAD", action.payload)
+        state.shoppingList = action.payload.data.shoppingList;
       })
       .addCase(addToShoppingList.rejected, handleRejected)
 
@@ -50,6 +48,7 @@ const shoppingListSlice = createSlice({
       .addCase(removeFromShoppingList.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
+		//   console.log("PAYLOAD DELETE", action.payload.data.shoppingList)
 		  state.shoppingList = action.payload.data.shoppingList
       })
       .addCase(removeFromShoppingList.rejected, handleRejected);
