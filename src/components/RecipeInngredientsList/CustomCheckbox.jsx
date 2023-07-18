2190bebe76fa5dca7f313c7d9b9c26a15d98fcdc
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-07-18 22:39:50 +0300

    Changed function

--- src/components/RecipeInngredientsList/CustomCheckbox.jsx
+++ src/components/RecipeInngredientsList/CustomCheckbox.jsx
@@ -10,7 +10,7 @@ const CustomCheckbox = ({ ingredient, shoppingList, recipeId }) => {
    
    const [checked, setChecked] = useState(false);
    const dispatch = useDispatch();
-   const { _id, measure } = ingredient;
+   const { _id: ingredientId, measure } = ingredient;
 
 //    useEffect(() => {
 //     if (shoppingList.length > 0) {
@@ -23,21 +23,51 @@ const CustomCheckbox = ({ ingredient, shoppingList, recipeId }) => {
 //   }, [shoppingList, dispatch, _id]);
 
 
-  const toggleCheckBox = ( product) => {
-    if (checked) {
-       dispatch(removeFromShoppingList(checked))
+//   const toggleCheckBox = ( product) => {
+//     if (checked) {
+//        dispatch(removeFromShoppingList(checked))
+//        setChecked(false);
+//     } else {
+//        dispatch(addToShoppingList(product))
+//        setChecked(true);
+//     }
+//   };
+   const toggleCheckBox = ({ measure, ingredientId, recipeId }) => {
+     if (shoppingList.length === 0) return;
+
+     const alreadyInSL = shoppingList.filter(
+       item => item._id === ingredientId && item.recipeId === recipeId
+     );
+     //   (item._id === ingredientId && item.recipeId === recipeId)
+
+     if (alreadyInSL.length > 0) {
+       dispatch(
+         removeFromShoppingList({
+           ingredientId: ingredientId,
+           recipeId: recipeId,
+           measure: measure,
+         })
+       );
        setChecked(false);
-    } else {
-       dispatch(addToShoppingList(product))
+     } else {
+       dispatch(
+         addToShoppingList({
+           ingredientId: ingredientId,
+           recipeId: recipeId,
+           measure: measure,
+         })
+       );
        setChecked(true);
-    }
-  };
+     }
+   };
+
+   
    
   return (
      <CheckBox
         type="checkbox"
         checked={checked}
-      onChange={() => toggleCheckBox({ measure, ingredientId:_id, recipeId})}
+      onChange={() => toggleCheckBox({ measure, ingredientId, recipeId})}
     >
      
     </CheckBox>
