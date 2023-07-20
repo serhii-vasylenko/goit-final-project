6bd824c576bbd8fd0edc53a53311df3f444e48bb
Jerrssey <fankton@gmail.com>
2023-07-20 21:27:54 +0300

    Update Header.styles.jsx

--- src/components/Header/Header.styles.jsx
+++ src/components/Header/Header.styles.jsx
@@ -1,30 +1,30 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 const HeaderEl = styled.header`
-position: absolute;
-left: 50%;
-transform: translate(-50%, 0);
-display: flex;
-align-items: center;
-min-width: 320px;
-margin: 0 auto;
-padding: 0 16px;
-padding-top: 18px;
-z-index: 10; // не видаляти
-@media (min-width: 375px) {
-		width: 375px;
-}
-@media (min-width: 768px) {
-	padding: 0 32px;
-	padding-top: 18px;
-	width: 768px;
-}
-@media (min-width: 1280px) {
-	padding: 0 20px;
-	padding-top: 18px;
-	width: 1280px;
-}
-`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  display: flex;
+  align-items: center;
+  min-width: 320px;
+  margin: 0 auto;
+  padding: 0 16px;
+  padding-top: 18px;
+  z-index: 10; // не видаляти
+  @media (min-width: 375px) {
+    width: 375px;
+  }
+  @media (min-width: 768px) {
+    padding: 0 32px;
+    padding-top: 18px;
+    width: 768px;
+  }
+  @media (min-width: 1280px) {
+    padding: 0 20px;
+    padding-top: 18px;
+    width: 1280px;
+  }
+`;
 const Logo = styled.img`
   width: 40px;
   height: 40px;
@@ -41,9 +41,9 @@ const Logo = styled.img`
 `;
 
 const Wrapper = styled.div`
-display: flex;
-align-items: center;
-margin-left: auto;
-`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+`;
 
-export {HeaderEl, Logo, Wrapper}
+export { HeaderEl, Logo, Wrapper };
